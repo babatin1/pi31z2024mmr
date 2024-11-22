cf21d7dd519476637581ee42091a2a6ed5b8f34d
Minnimukhametov Mansur <minn.mansur@gmail.com>
2024-11-22 08:18:34 -0500

    Figure

--- src/IFigure.php
+++ src/IFigure.php
@@ -26,3 +26,19 @@ abstract class Figure implements IFigure {
     public function getIcon(): string {
         switch ($this->color) {
             case Color::Black: $index = 0; break;
+            case Color::White: $index = 1; break;
+            default: return '';
+        }
+        return array_key_exists($index, $this->icon)
+            ? $this->icon[$index]
+            : '';
+    }
+
+    public function canMove(int $from_row, int $from_col, int $to_row, int $to_col, Board $board): bool {
+        return false;
+    }
+
+    public function canAttack(int $from_row, int $from_col, int $to_row, int $to_col, Board $board): bool {
+        return $this->canMove($from_row, $from_col, $to_row, $to_col, $board);
+    }
+}
