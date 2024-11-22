2a923c4826049b7d61d09c88a3cefdac4ffa2d2b
Minnimukhametov Mansur <minn.mansur@gmail.com>
2024-11-22 08:46:55 -0500

    Bishop

--- src/Bishop.php
+++ src/Bishop.php
@@ -7,3 +7,9 @@ class Bishop extends Figure {
 
     public function canMove(int $from_row, int $from_col, int $to_row, int $to_col, Board $board): bool {
         $diff_row = abs($to_row - $from_row);
+        $diff_col = abs($to_col - $from_col);
+        if ($diff_col != $diff_row) {
+            return false;
+        }
+        $step_col = ($to_col - $from_col) / $diff_col;
+        $step_row = ($to_row - $from_row) / $diff_row;
