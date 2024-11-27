6ff5ba007bfdb0e0f2c467d634564165474dc5ea
Minnimukhametov Mansur <minn.mansur@gmail.com>
2024-11-27 01:25:40 -0500

    Queen

--- src/Queen.php
+++ src/Queen.php
@@ -6,11 +6,36 @@ class Queen extends Figure {
     protected array $icon = ["\u{265B}", "\u{2655}"];
 
     public function canMove(int $from_row, int $from_col, int $to_row, int $to_col, Board $board): bool {
-        // Ферзь может ходить как по горизонтали/вертикали, так и по диагонали
-        $row_diff = abs($to_row - $from_row);
-        $col_diff = abs($to_col - $from_col);
-
-
-        return false; // Неправильный ход
+        $diff_row = abs($to_row - $from_row);
+        $diff_col = abs($to_col - $from_col);
+        $is_horizontal = $diff_row == 0;
+        $is_vertical = $diff_col == 0;
+        $is_diagonal = $diff_row == $diff_col;
+        if (!$is_diagonal && !$is_vertical && !$is_horizontal)
+            return false;
+        $step_col = $diff_col == 0
+            ? 0
+            : ($to_col - $from_col) / $diff_col;
+        $step_row = $diff_row == 0
+            ? 0
+            : ($to_row - $from_row) / $diff_row;
+        $start_col = $from_col;
+        $start_row = $from_row;
+        while ($start_col != $to_col || $start_row != $to_row) {
+            // echo 'C: ' . $start_col . ' - ' . $to_col . PHP_EOL;
+            // echo 'R: ' . $start_row . ' - ' . $to_row . PHP_EOL;
+            // echo 'Steps: C:' . $step_col . ' R: ' . $step_row . PHP_EOL;
+            $item = $board->getItem($start_row, $start_col);
+            if ($item && $item !== $this) {
+                return false;
+            }
+            // if ($start_col > 8 || $start_col < 0)
+            //     return false;
+            // if ($start_row > 8 || $start_row < 0)
+            //     return false;
+            $start_col += $step_col;
+            $start_row += $step_row;
+        }
+        return true;
     }
-}
+}.
