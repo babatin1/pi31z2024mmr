35bc4df9d35a358cb0e0a739e015fe398d6e4126
Minnimukhametov Mansur <minn.mansur@gmail.com>
2024-11-27 01:14:53 -0500

    king

--- src/King.php
+++ src/King.php
@@ -4,20 +4,12 @@ require_once('IFigure.php');
 
 class King extends Figure {
     protected array $icon = ["\u{265A}", "\u{2654}"];
-
-    public function canMove(int $from_row, int $from_col, int $to_row, int $to_col, Board $board): bool {
-        // Король может ходить максимум на одну клетку в любом направлении
-        $row_diff = abs($to_row - $from_row);
-        $col_diff = abs($to_col - $from_col);
-
-        if ($row_diff <= 1 && $col_diff <= 1) {
-            // Проверяем, что конечное положение не занято своей же фигурой
-            $target_item = $board->getItem($to_row, $to_col);
-            if ($target_item === null || $target_item->color !== $this->color) {
-                return true;
-            }
-        }
-
-        return false;
+    
+     public function canMove(int $from_row, int $from_col, int $to_row, int $to_col, Board $board): bool {
+        $diff_row = abs($to_row - $from_row);
+        $diff_col = abs($to_col - $from_col);
+        if ($diff_col > 1 || $diff_row > 1)
+            return false;
+        return true;
     }
 }
